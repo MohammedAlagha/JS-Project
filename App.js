5d8cb436861f14a064a2f01082eb46bd7dd9faf0
= <algha2013@gmail.com>
2019-10-03 12:19:08 +0300

    This project for GSG

---
+++ App.js
@@ -0,0 +1,53 @@
+
+function random(number) {
+    return Math.floor(Math.random()*number) ;
+}
+
+
+creategrid(16);             //run it when open the project first time
+
+const button = document.querySelector('#button');
+button.addEventListener("click", function () {
+    let n = prompt("Please enter n", 16)
+    creategrid(n)
+});
+
+
+
+function creategrid(n) {
+    const squares = document.getElementById("squares");
+    const squares_width = squares.offsetWidth;
+    const squares_height = squares.offsetHeight;
+    squares.innerText = '';
+    for (let i = 0; i < n; i++) {   
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < n; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+
+            cell.style.width = `${squares_width / n}px`;
+            cell.style.height = `${squares_height / n}px`;
+            row.appendChild(cell);
+        }
+        squares.appendChild(row);
+    }
+
+    const cells = document.getElementsByClassName("cell");
+
+    for (let cell of cells) {
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = `rgb(${random(256)},${random(256)},${random(256)})`;
+            console.log(cell.style.backgroundColor);
+        })
+    }
+
+}
+
+
+
+
+
+
+
+
